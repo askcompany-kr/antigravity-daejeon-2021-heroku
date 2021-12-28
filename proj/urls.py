55b2b200babaae6e3aa702c6c08fc16a102bdf9a
Chinseok Lee <me@askcompany.kr>
2021-12-28 09:45:56 +0900

    added MEDIA 설정

--- proj/urls.py
+++ proj/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -7,3 +9,5 @@ urlpatterns = [
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
